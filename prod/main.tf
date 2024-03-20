c73e1bcb73ae76008f19a2f9a9566c5d4eb440df
Mohamed Habib <moe.habib9@gmail.com>
2024-03-20 15:39:34 +0000

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -38,8 +38,8 @@ resource "aws_security_group" "security_group" {
   vpc_id      = aws_vpc.vpc_network.id
   name_prefix = "terraform-"
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
